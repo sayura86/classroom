fed8f2120799a64b947566837471742681c26e99
sayura86 <betterlatethannever52324@gmail.com>
2021-09-22 12:05:40 +0900

    ユーザー一覧

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,16 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.all
+    @newRoom = Room.new
   end
+
+  def show
+  end
+
+  def create
+    @room = Room.new(params[:room].permit(:title))
+    @room.save
+    redirect_to rooms_new_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'rooms/index'
+  get 'rooms/new'
+  get 'rooms/show/:id' => 'rooms#show', as: :rooms_show
+  post 'rooms/create' => 'rooms#create'
   root to: "rooms#index"
 end
